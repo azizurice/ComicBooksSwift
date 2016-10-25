de1839f7ed8fe10a78a5c422c5f6337116af5b50
Azizur <azizurice@gmail.com>
2016-10-25 11:51:46 -0700

    Added Objective C-Swift Bridign header

--- ComicBook/ComicBook-Bridging-Header.h
+++ ComicBook/ComicBook-Bridging-Header.h
@@ -5,6 +5,7 @@
 //  Created by Azizur Rahman on 2016-10-25.
 //  Copyright © 2016 letscodes. All rights reserved.
 //
+#import "MBProgressHUD.h"
 
 #ifndef ComicBook_Bridging_Header_h
 #define ComicBook_Bridging_Header_h
